5e6d68ee79cc1b18e04df64b0fce1a9a754b6ad1
XieRW <1429382875@qq.com>
2021-11-12 17:15:57 +0800

    fix--编码规范

--- cloud-auth/src/main/java/com/xrw/springCloudAlibaba/service/impl/UserFriendServiceImpl.java
+++ cloud-auth/src/main/java/com/xrw/springCloudAlibaba/service/impl/UserFriendServiceImpl.java
@@ -1,19 +1,10 @@
 package com.xrw.springCloudAlibaba.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xrw.springCloudAlibaba.dao.UserFriendDao;
-import com.xrw.springCloudAlibaba.entity.SysUserEntity;
 import com.xrw.springCloudAlibaba.entity.UserFriendEntity;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @Description: 好友表服务层
