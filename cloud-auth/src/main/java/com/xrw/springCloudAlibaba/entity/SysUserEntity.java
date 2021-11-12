5cd6a31027dcd0b0ce7e3a6f4a1b23f0edddc727
XieRW <1429382875@qq.com>
2021-11-12 17:17:28 +0800

    fix--编码规范

--- cloud-auth/src/main/java/com/xrw/springCloudAlibaba/entity/SysUserEntity.java
+++ cloud-auth/src/main/java/com/xrw/springCloudAlibaba/entity/SysUserEntity.java
@@ -8,6 +8,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
 import java.util.Date;
 
 /**
@@ -18,68 +19,68 @@ import java.util.Date;
 @Accessors(chain = true)
 @TableName("user")
 public class SysUserEntity extends Model<SysUserEntity> {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 用户ID
-	 */
-	@TableId
-	private Long id;
-	private Integer revision;
-	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
-	private Date createTime;
-	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
-	private Date updateTime;
-	private Long createUserId;
-	private Long updateUserId;
-	@TableLogic
-	private Integer isDeleted;
+    /**
+     * 用户ID
+     */
+    @TableId
+    private Long id;
+    private Integer revision;
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+    private Long createUserId;
+    private Long updateUserId;
+    @TableLogic
+    private Integer isDeleted;
 
-	/**
-	 * 用户登录名
-	 */
-	private String username;
-	/**
-	 * 用户昵称
-	 */
-	private String name;
+    /**
+     * 用户登录名
+     */
+    private String username;
+    /**
+     * 用户昵称
+     */
+    private String name;
 
-	/**
-	 * 密码
-	 */
-	@JSONField(serialize = false)
-	private String password;
+    /**
+     * 密码
+     */
+    @JSONField(serialize = false)
+    private String password;
 
-	/**
-	 * 盐
-	 */
-	@JSONField(serialize = false)
-	private String salt;
+    /**
+     * 盐
+     */
+    @JSONField(serialize = false)
+    private String salt;
 
-	/**
-	 * 邮箱
-	 */
-	private String email;
+    /**
+     * 邮箱
+     */
+    private String email;
 
-	/**
-	 * 手机号
-	 */
-	private String mobile;
+    /**
+     * 手机号
+     */
+    private String mobile;
 
-	/**
-	 * 角色ID
-	 */
-	private Long roleId;
-	@TableField(exist=false)
-	private String roleName;
-	@TableField(exist=false)
-	private String sourceUserId;
-	private String sourceType;
-	/**
-	 * 状态  0：禁用   1：正常  2：
-	 */
-	@JSONField(serialize = false)
-	private Integer status;
-	@TableField(exist=false)
-	private String token;
+    /**
+     * 角色ID
+     */
+    private Long roleId;
+    @TableField(exist = false)
+    private String roleName;
+    @TableField(exist = false)
+    private String sourceUserId;
+    private String sourceType;
+    /**
+     * 状态  0：禁用   1：正常  2：
+     */
+    @JSONField(serialize = false)
+    private Integer status;
+    @TableField(exist = false)
+    private String token;
 }
