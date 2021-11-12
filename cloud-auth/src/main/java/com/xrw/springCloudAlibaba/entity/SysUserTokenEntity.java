64a0b8f097aa5a012776e1ed3188b46a134bca87
XieRW <1429382875@qq.com>
2021-11-12 17:18:05 +0800

    fix--编码规范

--- cloud-auth/src/main/java/com/xrw/springCloudAlibaba/entity/SysUserTokenEntity.java
+++ cloud-auth/src/main/java/com/xrw/springCloudAlibaba/entity/SysUserTokenEntity.java
@@ -17,16 +17,24 @@ import java.util.Date;
 @Data
 @TableName("sys_user_token")
 public class SysUserTokenEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	//用户ID
-	@TableId(type = IdType.INPUT)
-	private Long userId;
-	//token
-	private String token;
-	//过期时间
-	private Date expireTime;
-	//更新时间
-	private Date updateTime;
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户ID
+     */
+    @TableId(type = IdType.INPUT)
+    private Long userId;
+    /**
+     * token
+     */
+    private String token;
+    /**
+     * 过期时间
+     */
+    private Date expireTime;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
 
 }
