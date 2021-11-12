09e86a56116e05814c5858ca0de0b4ee1a4af4c7
XieRW <1429382875@qq.com>
2021-11-12 17:17:17 +0800

    fix--编码规范

--- cloud-auth/src/main/java/com/xrw/springCloudAlibaba/entity/SysRoleEntity.java
+++ cloud-auth/src/main/java/com/xrw/springCloudAlibaba/entity/SysRoleEntity.java
@@ -14,28 +14,29 @@ import java.util.Date;
 
 /**
  * TODO 系统用户
+ *
  * @author xearin
  * @date 2019-05-22 14:54:50
  */
 @Data
 @TableName("role")
 public class SysRoleEntity extends Model<SysRoleEntity> {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 用户ID
-	 */
-	@TableId
-	private Long id;
-	@TableLogic
-	private Integer isDeleted;
-	private Integer revision;
-	private Long createUserId;
-	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
-	private Date createTime;
-	private Long updateUserId;
-	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
-	private Date updateTime;
-	private String name;
-	private String remark;
+    /**
+     * 用户ID
+     */
+    @TableId
+    private Long id;
+    @TableLogic
+    private Integer isDeleted;
+    private Integer revision;
+    private Long createUserId;
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+    private Long updateUserId;
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+    private String name;
+    private String remark;
 }
