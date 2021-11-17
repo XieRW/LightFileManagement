51f6a86e3e7238ab34092c751c76be830913e08e
XieRW <1429382875@qq.com>
2021-11-17 19:10:42 +0800

    fix--规范化编码

--- cloud-auth/src/main/java/com/xrw/springCloudAlibaba/dao/MailContactorDao.java
+++ cloud-auth/src/main/java/com/xrw/springCloudAlibaba/dao/MailContactorDao.java
@@ -12,79 +12,80 @@ import java.util.List;
 
 /**
  * 联系人表
- * 
+ *
  * @author xearin
  * @email sunlightcs@gmail.com
  * @date 2019-05-25 13:33:40
  */
 @Mapper
 public interface MailContactorDao extends BaseMapper<MailContactorEntity> {
-	IPage<MailContactorEntity> selectContactir(IPage<MailContactorEntity> pagination, @Param("groupIds") Long[] groupIds, @Param("search") String search, @Param("platformId") Long platformId, @Param("plural") String[] plural);
+    IPage<MailContactorEntity> selectContactir(IPage<MailContactorEntity> pagination, @Param("groupIds") Long[] groupIds, @Param("search") String search, @Param("platformId") Long platformId, @Param("plural") String[] plural);
 
-	List<MailContactorEntity> selectContactir(@Param("groupIds") Long[] groupIds, @Param("search") String search, @Param("platformId") Long platformId);
+    List<MailContactorEntity> selectContactir(@Param("groupIds") Long[] groupIds, @Param("search") String search, @Param("platformId") Long platformId);
 
-	IPage<MailContactorEntity> selectCommonContact(IPage<MailContactorEntity> pagination, @Param("search") String search, @Param("platformId") Long platformId);
+    IPage<MailContactorEntity> selectCommonContact(IPage<MailContactorEntity> pagination, @Param("search") String search, @Param("platformId") Long platformId);
 
-	int insertContactor(@Param("contactor") MailContactorEntity mailContactorEntity);
+    int insertContactor(@Param("contactor") MailContactorEntity mailContactorEntity);
 
-	int insertContactorBath(List<MailContactorEntity> contactors);
+    int insertContactorBath(List<MailContactorEntity> contactors);
 
-	MailContactorEntity selectByRpPoliceResponseId(@Param("responseId") Long responseId);
+    MailContactorEntity selectByRpPoliceResponseId(@Param("responseId") Long responseId);
 
-	List<MailContactorEntity> selectContactirByGroupId(@Param("groupId") Long groupId);
+    List<MailContactorEntity> selectContactirByGroupId(@Param("groupId") Long groupId);
 
-	MailContactorEntity selectContactorAndGroupIdsById(@Param("contactorId") Long contactorId);
+    MailContactorEntity selectContactorAndGroupIdsById(@Param("contactorId") Long contactorId);
 
 //	@Select("select sp.* from mail_contactor mc left join sys_platform sp on mc.platform_id=sp.id where mc.open_id = #{openId} and mc.is_deleted = 0 ")
 //	List<SysPlatformEntity> selectPlatformByOpenId(@Param("openId")String openId);
 
-	MailContactorEntity selectOneByOpenId(@Param("openId") String openId, @Param("platformId") Long platformId);
+    MailContactorEntity selectOneByOpenId(@Param("openId") String openId, @Param("platformId") Long platformId);
 
     MailContactorEntity selectOneById(@Param("id") Long id, @Param("platformId") Long platformId);
 
-	List<MailContactorEntity> selectContactorAndGroupIdsByOpenId(@Param("openId") String openId);
+    List<MailContactorEntity> selectContactorAndGroupIdsByOpenId(@Param("openId") String openId);
 
 //	@Select("select name,position,work_unit,id,mobile1,mobile2 from mail_contactor WHERE id IN(SELECT receiver_user FROM `message_infor` where create_user=#{userId}  GROUP BY receiver_user)")
 //	@Select("select a.phone,b.name,b.position,b.work_unit,b.mobile1,b.mobile2 from(SELECT phone FROM `message_infor` where create_user=#{userId} GROUP BY phone)a LEFT JOIN mail_contactor b ON a.phone=b.mobile1 AND b.is_deleted=0")
 
-	List<MailContactorEntity> selectContactorByMessageReceiverId(Page<MailContactorEntity> page, String search,
+    List<MailContactorEntity> selectContactorByMessageReceiverId(Page<MailContactorEntity> page, String search,
                                                                  Long userId);
 
-	@Select("<script>"
-    		+"select * from mail_contactor where id in(select contacts_id from mail_contactor_group where group_id in"
+    @Select("<script>"
+            + "select * from mail_contactor where id in(select contacts_id from mail_contactor_group where group_id in"
             + "<foreach item='id' index='index' collection='ids'      open='(' separator=',' close=')'>"
             + "#{id}"
             + "</foreach>"
-            +")"
+            + ")"
             + "</script>")
-	List<MailContactorEntity> getListInGroup(@Param("ids") List<String> ids);
+    List<MailContactorEntity> getListInGroup(@Param("ids") List<String> ids);
 
 
     @Select("<script>"
-    		+"select * from mail_contactor where id in(select mail_contactor_id from message_contactor_group <if test='ids!=null and ids.size()>0'> where message_group_id in"
+            + "select * from mail_contactor where id in(select mail_contactor_id from message_contactor_group <if test='ids!=null and ids.size()>0'> where message_group_id in"
             + "<foreach item='id' index='index' collection='ids'      open='(' separator=',' close=')'>"
             + "#{id}"
             + "</foreach>"
-			+ "</if>"
-            +")"
+            + "</if>"
+            + ")"
             + "</script>")
-	List<MailContactorEntity> getListInMessageGroup(@Param("ids") List<String> ids);
+    List<MailContactorEntity> getListInMessageGroup(@Param("ids") List<String> ids);
 
     @Select("select * from mail_contactor where id in(select contactor_id from rp_unit_contactor where unit_id=#{id}) and is_deleted=0 ")
-	List<MailContactorEntity> getListInRpUnit(@Param("id") long id);
-    @Select("select * from mail_contactor where id in(select contactor_id from resoure_protect_contactor where protect_id=#{id} and type=#{type})")
-	List<MailContactorEntity> listByProtectIdAndType(@Param("id") Long id, @Param("type") int type);
+    List<MailContactorEntity> getListInRpUnit(@Param("id") long id);
 
-	List<MailContactorEntity> selectContactorListNoPage(@Param("groupIds") List<Long> groupIds,
+    @Select("select * from mail_contactor where id in(select contactor_id from resoure_protect_contactor where protect_id=#{id} and type=#{type})")
+    List<MailContactorEntity> listByProtectIdAndType(@Param("id") Long id, @Param("type") int type);
+
+    List<MailContactorEntity> selectContactorListNoPage(@Param("groupIds") List<Long> groupIds,
                                                         @Param("platformId") Long platformId);
 
     List<MailContactorEntity> contactorInfo(@Param("contactIds") List<String> contactIds);
 
     MailContactorEntity getDutyContactor(@Param("id") Long id);
 
-	MailContactorEntity getBySourceIdAndType(@Param("sourceId") String sourceId, @Param("sourceType") String sourceType);
+    MailContactorEntity getBySourceIdAndType(@Param("sourceId") String sourceId, @Param("sourceType") String sourceType);
 
-	MailContactorEntity getByMobile(@Param("mobile") String mobile);
+    MailContactorEntity getByMobile(@Param("mobile") String mobile);
 
-	List<MailContactorEntity> getListByMobile(@Param("mobile") String mobile);
+    List<MailContactorEntity> getListByMobile(@Param("mobile") String mobile);
 }
