b4c553c7fc0afc696d24767a31920a958febc7d7
XieRW <1429382875@qq.com>
2021-11-18 17:20:17 +0800

    feat--更新file-structure.sql

--- database/file/structure.sql
+++ database/file/structure.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS file;
 CREATE TABLE file(
-    id bigint NOT NULL AUTO_INCREMENT  COMMENT '主键' ,
+    id bigint(64) NOT NULL AUTO_INCREMENT  COMMENT '主键' ,
     is_deleted INT(1)   DEFAULT 0 COMMENT '逻辑删除;0:未删除，1：已删除' ,
-    revision bigint    COMMENT '乐观锁' ,
-    create_user_id bigint    COMMENT '创建人' ,
+    revision bigint(64)    COMMENT '乐观锁' ,
+    create_user_id bigint(64)    COMMENT '创建人' ,
     create_time DATETIME   DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' ,
-    update_user_id bigint    COMMENT '更新人' ,
+    update_user_id bigint(64)    COMMENT '更新人' ,
     update_time DATETIME   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间' ,
     size INT    COMMENT '文件大小' ,
     filename VARCHAR(255)    COMMENT '文件实际名称' ,
@@ -15,8 +15,10 @@ CREATE TABLE file(
     extension VARCHAR(255)    COMMENT '文件扩展名' ,
     remark VARCHAR(255)    COMMENT '文件备注' ,
     directory VARCHAR(255)    COMMENT '文件分类目录' ,
+    file_group_id bigint(64)    COMMENT '文件分组' ,
     PRIMARY KEY (id)
 )  COMMENT = '文件表';
+
 
 DROP TABLE IF EXISTS file_share_friend;
 CREATE TABLE file_share_friend(
@@ -34,3 +36,17 @@ CREATE TABLE file_share_friend(
     PRIMARY KEY (id)
 )  COMMENT = '文件共享表';
 
+DROP TABLE IF EXISTS file_group;
+CREATE TABLE file_group(
+    id bigint(64) NOT NULL AUTO_INCREMENT  COMMENT '主键' ,
+    is_deleted INT(1)   DEFAULT 0 COMMENT '逻辑删除;0:未删除，1：已删除' ,
+    revision bigint(64)    COMMENT '乐观锁' ,
+    create_user_id bigint(64)    COMMENT '创建人' ,
+    create_time DATETIME   DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' ,
+    update_user_id bigint(64)    COMMENT '更新人' ,
+    update_time DATETIME   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间' ,
+    p_id bigint(64)   DEFAULT 0 COMMENT '父id' ,
+    name VARCHAR(255)    COMMENT '分组名' ,
+    detail VARCHAR(255)    COMMENT '分组说明' ,
+    PRIMARY KEY (id)
+)  COMMENT = '文件分组表';
