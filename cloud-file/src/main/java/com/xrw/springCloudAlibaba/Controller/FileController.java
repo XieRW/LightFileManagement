10d42c3cdcf35b810a62b21d282fa3f5e7c6ed1e
XieRW <1429382875@qq.com>
2021-11-22 10:08:43 +0800

    feat--构建file模块的整体结构

--- cloud-file/src/main/java/com/xrw/springCloudAlibaba/Controller/FileController.java
+++ cloud-file/src/main/java/com/xrw/springCloudAlibaba/Controller/FileController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @program: LightFileManagement
  * @description: 用户好友控制器层
  * @author: 谢荣旺 1429382875@qq.com
- * @create: 2021-11-12 10:30
+ * @create: 2021-11-22 10:30
  **/
 @Slf4j
 @RestController
