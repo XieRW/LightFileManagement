3822b29c2c84b8064d1dd3ec5ec4db8fa0e48abc
XieRW <1429382875@qq.com>
2021-11-23 16:21:57 +0800

    feat--开发文件上传、下载、删除、查看接口

--- cloud-file/src/main/java/com/xrw/springCloudAlibaba/service/FileServiceImpl.java
+++ cloud-file/src/main/java/com/xrw/springCloudAlibaba/service/FileServiceImpl.java
@@ -47,30 +47,30 @@ public class FileServiceImpl extends ServiceImpl<FileDao, FileEntity> {
     private ThreadPoolExecutor threadPoolExecutor;
 
 
-    public List<FileEntity> upload(MultipartFile[] files,String type,String remake,Long fileGroupId,Long userId){
+    public List<FileEntity> upload(MultipartFile[] files, String type, String remake, Long fileGroupId, Long userId) {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM");
         String month = df.format(new Date());
-        String fileTypeMiddlePath = SysDictItemEnum.getByKey(SysDictEnum.file_type.getKey(),type).getValue();
-        fileTypeMiddlePath += month+"/";
+        String fileTypeMiddlePath = SysDictItemEnum.getByKey(SysDictEnum.file_type.getKey(), type).getValue();
+        fileTypeMiddlePath += month + "/";
         List<FileEntity> entities = new ArrayList<>();
-        for (MultipartFile file:files) {
+        for (MultipartFile file : files) {
             df = new SimpleDateFormat("yyyyMMddHHmmssSSS");
             String originalFileName = file.getOriginalFilename();
-            if (originalFileName.indexOf("\\")>=0){
-                originalFileName = originalFileName.substring(originalFileName.lastIndexOf("\\")+1);
+            if (originalFileName.indexOf("\\") >= 0) {
+                originalFileName = originalFileName.substring(originalFileName.lastIndexOf("\\") + 1);
             }
             String fileName;
             String extension = "";
-            if (originalFileName.lastIndexOf(".")>=0){
+            if (originalFileName.lastIndexOf(".") >= 0) {
                 fileName = df.format(new Date()) + originalFileName.substring(originalFileName.lastIndexOf("."));
                 extension = originalFileName.substring(originalFileName.lastIndexOf("."));
-            }else {
+            } else {
                 fileName = df.format(new Date()) + originalFileName;
             }
             String filePath = fileUploadPath + fileTypeMiddlePath + fileName;
             File serverFile = new File(filePath);
-            if (!serverFile.getParentFile().exists()){
-                if (!serverFile.getParentFile().mkdirs()){
+            if (!serverFile.getParentFile().exists()) {
+                if (!serverFile.getParentFile().mkdirs()) {
                     throw new ApiException(ApiError.FILE_MKDIR_ERROR);
                 }
             }
@@ -98,16 +98,16 @@ public class FileServiceImpl extends ServiceImpl<FileDao, FileEntity> {
         return entities;
     }
 
-    public boolean delete(Long id,Long userId){
+    public boolean delete(Long id, Long userId) {
         FileEntity fileEntity = baseMapper.selectById(id);
-        if (fileEntity==null){
+        if (fileEntity == null) {
             throw new ApiException(ApiError.DATA_NOT_EXISTS);
         }
         //todo 添加文件共享功能之后，此时的权限判断条件需要做相应的更改
-        if (!fileEntity.getUserId().equals(userId)){
+        if (!fileEntity.getUserId().equals(userId)) {
             throw new ApiException(ApiError.INTERFACE_UNPREMITTED);
         }
-        String filePath = fileUploadPath+fileEntity.getPath();
+        String filePath = fileUploadPath + fileEntity.getPath();
         File file = new File(filePath);
         Future<Boolean> submit = threadPoolExecutor.submit(() -> delFile(file));
         try {
@@ -115,21 +115,21 @@ public class FileServiceImpl extends ServiceImpl<FileDao, FileEntity> {
         } catch (InterruptedException | ExecutionException e) {
             throw new ApiException(ApiError.FILE_DELETE_ERROR);
         }
-        if (baseMapper.deleteById(id)>0){
+        if (baseMapper.deleteById(id) > 0) {
             return true;
-        }else {
+        } else {
             throw new ApiException(ApiError.FILE_DELETE_ERROR);
         }
     }
 
     /**
-     * @Description: 删除一个文件或者逐级删除文件夹
      * @param file: 文件或者文件夹
+     * @Description: 删除一个文件或者逐级删除文件夹
      * @return: boolean
      * @Author: xearin 1429382875@qq.com
      * @Date: 2021/11/23
      */
-    public boolean delFile(File file){
+    public boolean delFile(File file) {
         if (!file.isFile()) {
             File[] files = file.listFiles();
             assert files != null;
@@ -139,19 +139,19 @@ public class FileServiceImpl extends ServiceImpl<FileDao, FileEntity> {
                 }
             }
         }
-        if (!file.delete()){
+        if (!file.delete()) {
             throw new ApiException(ApiError.FILE_DELETE_ERROR);
         }
         return true;
     }
 
-    public void download(Long id, Long userId, boolean isForceDownload,HttpServletResponse response){
+    public void download(Long id, Long userId, boolean isForceDownload, HttpServletResponse response) {
         FileEntity fileEntity = baseMapper.selectById(id);
-        if (fileEntity==null){
+        if (fileEntity == null) {
             throw new ApiException(ApiError.DATA_NOT_EXISTS);
         }
         //todo 添加文件共享功能之后，此时的权限判断条件需要做相应的更改
-        if (!fileEntity.getUserId().equals(userId)){
+        if (!fileEntity.getUserId().equals(userId)) {
             throw new ApiException(ApiError.INTERFACE_UNPREMITTED);
         }
         File file = new File(fileUploadPath + fileEntity.getPath());
@@ -167,28 +167,28 @@ public class FileServiceImpl extends ServiceImpl<FileDao, FileEntity> {
                 response.setContentType("application/force-download");
             }
             //设置文件名
-            response.setHeader("Content-disposition","attachment;filename="+ URLEncoder.encode(fileEntity.getFilename(),"UTF-8"));
+            response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(fileEntity.getFilename(), "UTF-8"));
             fis = new FileInputStream(file);
             bis = new BufferedInputStream(fis);
             OutputStream os = response.getOutputStream();
             int i = bis.read(buffer);
-            while (i != -1){
-                os.write(buffer,0,i);
-                i=bis.read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = bis.read(buffer);
             }
         } catch (IOException e) {
             throw new ApiException(ApiError.FILE_GET_ERROR);
-        }finally {
+        } finally {
             //关闭输入流
-            if (bis != null){
+            if (bis != null) {
                 try {
                     bis.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     log.info(e.getMessage());
                 }
             }
             //关闭输入流
-            if (fis!=null){
+            if (fis != null) {
                 try {
                     fis.close();
                 } catch (IOException e) {
@@ -198,13 +198,13 @@ public class FileServiceImpl extends ServiceImpl<FileDao, FileEntity> {
         }
     }
 
-    public Page<FileEntity> getSelectPage(Long page,Long size,String select,Long fileGroupId,Long userId){
+    public Page<FileEntity> getSelectPage(Long page, Long size, String select, Long fileGroupId, Long userId) {
         Long offset = 0L;
         if (page != null && size != null) {
             offset = (page - 1) * size;
         }
-        ArrayList<FileEntity> applicationEntities = baseMapper.getSelectPage(offset,size,select,fileGroupId,userId);
-        Long pageCount = baseMapper.getSelectPageCount(offset,size,select,fileGroupId,userId);
+        ArrayList<FileEntity> applicationEntities = baseMapper.getSelectPage(offset, size, select, fileGroupId, userId);
+        Long pageCount = baseMapper.getSelectPageCount(offset, size, select, fileGroupId, userId);
         Page<FileEntity> entityPage = new Page<>(Optional.ofNullable(page).orElse(0L),
                 Optional.ofNullable(size).orElse(0L),
                 Optional.ofNullable(pageCount).orElse(0L));
