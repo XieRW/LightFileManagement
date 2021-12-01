a537992345d522694fd33de8afaefccaf94973d1
XieRW <1429382875@qq.com>
2021-12-01 17:23:42 +0800

    feat--配置X-Frame-Options，避免嵌入iframe失败

--- cloud-auth/src/main/java/com/xrw/springCloudAlibaba/config/filter/AddResponseHeaderFilter.java
+++ cloud-auth/src/main/java/com/xrw/springCloudAlibaba/config/filter/AddResponseHeaderFilter.java
@@ -22,6 +22,11 @@ public class AddResponseHeaderFilter extends OncePerRequestFilter {
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         response.setHeader("X-Frame-Options", "SAMEORIGIN");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,token");
         filterChain.doFilter(request, response);
     }
 
